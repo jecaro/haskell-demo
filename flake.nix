48aa5dc7033aa6dbee68f0a6ddab7ce2a184879f
jecaro <jeancharles.quillet@gmail.com>
2026-01-14 10:51:40 +0100

    Bump to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
@@ -9,12 +9,11 @@
         pkgs.mkShell {
           buildInputs = [
             pkgs.ghcid
-            (pkgs.haskellPackages.ghcWithPackages (pkgs_: with pkgs_;
-            [
-              cabal-install
-              haskell-language-server
-              hlint
-              implicit-hie
+            (pkgs.haskellPackages.ghcWithPackages (pkgs_: [
+              pkgs_.cabal-install
+              pkgs_.haskell-language-server
+              pkgs_.hlint
+              pkgs_.implicit-hie
             ]))
           ];
         };
