c19cd4966c676a95d07814642ddae0b7205ef1d1
jecaro <jeancharles.quillet@gmail.com>
2026-01-22 18:04:45 +0100

    Add a simple derivation for CI

--- flake.nix
+++ flake.nix
@@ -5,6 +5,21 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+        pname = "haskell-demo";
+        version = "0.0.1";
+        src = self;
+        buildInputs = [ pkgs.haskellPackages.ghc ];
+        buildPhase = ''
+          ghc Main.hs -o haskell-demo
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp haskell-demo $out/bin/
+        '';
+
+      };
+
       devShell.x86_64-linux =
         pkgs.mkShell {
           buildInputs = [
